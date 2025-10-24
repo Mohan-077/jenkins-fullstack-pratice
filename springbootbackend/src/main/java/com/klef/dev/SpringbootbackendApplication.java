f8c634f8c212c1561f7b663ee8fafa316b210cd2
Mohan <mohanbattula732@gamil.com>
2025-10-24 10:55:58 +0530

    updated backend

--- springbootbackend/src/main/java/com/klef/dev/SpringbootbackendApplication.java
+++ springbootbackend/src/main/java/com/klef/dev/SpringbootbackendApplication.java
@@ -2,14 +2,15 @@ package com.klef.dev;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringbootbackendApplication 
+public class SpringBootProjectApplication extends SpringBootServletInitializer
 {
-	public static void main(String[] args) 
-	{
-		SpringApplication.run(SpringbootbackendApplication.class, args);
-		System.out.println("Project Backend is Running Successfully ....");
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProjectApplication.class, args);
+		System.out.println("Project is Running ...");
 	}
-
+	
 }
